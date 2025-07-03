ccb7d06bdfe4c88d06ce46edb06f4dcfc328a52a
kijv <93486795+kijv@users.noreply.github.com>
2025-07-03 16:01:19 -0400

    fix next-font vite

--- packages/vite/src/index.ts
+++ packages/vite/src/index.ts
@@ -1,36 +1,43 @@
-// @ts-ignore
-
-import path from 'node:path';
-import { dataToEsm, normalizePath } from '@rollup/pluginutils';
-import escodegen from 'escodegen-wallaby';
-import MagicString from 'magic-string';
+import path from "node:path";
+import { dataToEsm, normalizePath } from "@rollup/pluginutils";
+import escodegen from "escodegen-wallaby";
+import MagicString from "magic-string";
 import {
   isCSSRequest,
   type PluginOption,
   type ResolvedConfig,
   type ViteDevServer,
-} from 'vite';
-import { visit } from './ast/transform';
-import type { FontLoader } from './declarations';
-import { compileTargetCss } from './target-css';
-import { importResolve, removeQuerySuffix, tryCatch } from './utils';
+} from "vite";
+import { visit } from "./ast/transform";
+import type { FontLoader } from "./declarations";
+import { compileTargetCss } from "./target-css";
+import { importResolve, removeQuerySuffix, tryCatch } from "./utils";
+import {
+  renderAssetUrlInJS,
+  //   assetUrlRE,
+  //   publicAssetUrlRE,
+  //   publicAssetUrlCache,
+} from "@vitejs/vite/packages/vite/src/node/plugins/asset";
+// import { encodeURIPath } from "@vitejs/vite/packages/vite/src/node/utils";
+// import { toOutputFilePathInCss } from "@vitejs/vite/packages/vite/src/node/build";
+// import { slash, cleanUrl } from "@vitejs/vite/packages/vite/src/shared/utils";
 
 const plugin = (): PluginOption[] => {
   const rewriteNextFontImport = {
-    '@next/font/google': 'next-font/google',
-    '@next/font/local': 'next-font/local',
-    'next/font/google': 'next-font/google',
-    'next/font/local': 'next-font/local',
+    "@next/font/google": "next-font/google",
+    "@next/font/local": "next-font/local",
+    "next/font/google": "next-font/google",
+    "next/font/local": "next-font/local",
   };
-  const importedLoaders = ['next-font/google', 'next-font/local'];
+  const importedLoaders = ["next-font/google", "next-font/local"];
   const fontLoaders: [string, Promise<FontLoader>][] = [
     [
-      'next-font/google/target.css',
-      import('./google').then((mod) => mod.default),
+      "next-font/google/target.css",
+      import("./google").then((mod) => mod.default),
     ],
     [
-      'next-font/local/target.css',
-      import('./local').then((mod) => mod.default),
+      "next-font/local/target.css",
+      import("./local").then((mod) => mod.default),
     ],
   ] as const;
   const loaderCache = {
@@ -56,8 +63,8 @@ const plugin = (): PluginOption[] => {
 
   return [
     {
-      name: 'next-font:scan',
-      enforce: 'pre',
+      name: "next-font:scan",
+      enforce: "pre",
       configureServer(server) {
         servers.push(server);
       },
@@ -68,7 +75,7 @@ const plugin = (): PluginOption[] => {
       },
     },
     {
-      name: 'next-font:transform',
+      name: "next-font:transform",
       async transform(code, id) {
         if (!/\.(?:j|t)sx?$|\.mjs$/.test(id)) return null;
 
@@ -91,11 +98,11 @@ const plugin = (): PluginOption[] => {
 
         if (!changed) return;
 
-        const s = new MagicString('');
+        const s = new MagicString("");
 
         for (const node of ast.body) {
           s.append(escodegen.generate(node));
-          s.append('\n');
+          s.append("\n");
         }
         return {
           code: s.toString(),
@@ -103,8 +110,8 @@ const plugin = (): PluginOption[] => {
       },
     },
     {
-      name: 'next-font:generate:pre',
-      enforce: 'pre',
+      name: "next-font:generate:pre",
+      enforce: "pre",
       configureServer(server) {
         return () => {
           server.middlewares.use((req, res, next) => {
@@ -118,11 +125,11 @@ const plugin = (): PluginOption[] => {
       async load(this, id, opts) {
         if (!/\.css(?:$|\?)/.test(id)) return null;
         const { data: resolvedId, error } = await tryCatch(
-          importResolve(removeQuerySuffix(id)),
+          importResolve(removeQuerySuffix(id))
         );
         if (error) return null;
         const pair = fontLoaders.find(
-          (id) => import.meta.resolve(id[0]) === resolvedId,
+          (id) => import.meta.resolve(id[0]) === resolvedId
         );
         if (!pair) return null;
 
@@ -132,7 +139,7 @@ const plugin = (): PluginOption[] => {
           loaderCache,
           fileCache,
           ctx: this,
-          isDev: config?.command === 'serve',
+          isDev: config?.command === "serve",
           isServer: opts?.ssr ?? false,
           config: config!,
         });
@@ -141,8 +148,8 @@ const plugin = (): PluginOption[] => {
       },
     },
     {
-      name: 'next-font:generate:post',
-      enforce: 'post',
+      name: "next-font:generate:post",
+      enforce: "post",
       async transform(this, _code, id, opts) {
         const targetCss = targetCssCache.get(id);
 
@@ -158,10 +165,10 @@ const plugin = (): PluginOption[] => {
             ? new MagicString(css).generateMap({ hires: true })
             : undefined;
 
-          if (config?.command === 'serve' && !opts?.ssr) {
+          if (config?.command === "serve" && !opts?.ssr) {
             const code = [
               `import { updateStyle as __vite__updateStyle, removeStyle as __vite__removeStyle } from ${JSON.stringify(
-                path.posix.join(config.base, '/@vite/client'),
+                path.posix.join(config.base, "/@vite/client")
               )}`,
               `const __vite__id = ${JSON.stringify(id)}`,
               `const __vite__css = ${JSON.stringify(css)}`,
@@ -169,9 +176,9 @@ const plugin = (): PluginOption[] => {
               // css modules exports change on edit so it can't self accept
               `${
                 modulesCode ||
-                'import.meta.hot.accept()\nimport.meta.hot.prune(() => __vite__removeStyle(__vite__id))'
+                "import.meta.hot.accept()\nimport.meta.hot.prune(() => __vite__removeStyle(__vite__id))"
               }`,
-            ].join('\n');
+            ].join("\n");
             return {
               code,
               map,
@@ -189,15 +196,15 @@ const plugin = (): PluginOption[] => {
       },
     },
     {
-      name: 'next-font:generate:build',
-      apply: 'build',
-      enforce: 'post',
+      name: "next-font:generate:build",
+      apply: "build",
+      enforce: "post",
       async renderChunk(code, chunk, opts) {
         let chunkCSS = Array.from(styles.values()).join();
 
         function ensureFileExt(name: string, ext: string) {
           return normalizePath(
-            path.format({ ...path.parse(name), base: undefined, ext }),
+            path.format({ ...path.parse(name), base: undefined, ext })
           );
         }
 
@@ -205,8 +212,8 @@ const plugin = (): PluginOption[] => {
 
         if (chunkCSS) {
           if (this.environment.config.build.cssCodeSplit) {
-            if (opts.format === 'es' || opts.format === 'cjs') {
-              const cssFullAssetName = ensureFileExt(chunk.name, '.css');
+            if (opts.format === "es" || opts.format === "cjs") {
+              const cssFullAssetName = ensureFileExt(chunk.name, ".css");
               // if facadeModuleId doesn't exist or doesn't have a CSS extension,
               // that means a JS entry file imports a CSS file.
               // in this case, only use the filename for the CSS chunk name like JS chunks.
@@ -218,15 +225,15 @@ const plugin = (): PluginOption[] => {
 
               // emit corresponding css file
               const referenceId = this.emitFile({
-                type: 'asset',
+                type: "asset",
                 name: cssAssetName,
                 // originalFileName,
                 source: chunkCSS,
               });
               chunk.viteMetadata!.importedCss.add(
-                this.getFileName(referenceId),
+                this.getFileName(referenceId)
               );
-            } else if (this.environment.config.consumer === 'client') {
+            } else if (this.environment.config.consumer === "client") {
               // legacy build and inline css
 
               // Entry chunk CSS will be collected into `chunk.viteMetadata.importedCss`
@@ -235,7 +242,6 @@ const plugin = (): PluginOption[] => {
               // But because entry chunk can be imported by dynamic import,
               // we shouldn't remove the inlined CSS. (#10285)
 
-              chunkCSS = await finalizeCss(chunkCSS, true, config);
               let cssString = JSON.stringify(chunkCSS);
               cssString =
                 renderAssetUrlInJS(this, chunk, opts, cssString)?.toString() ||
@@ -246,12 +252,12 @@ const plugin = (): PluginOption[] => {
                 `${style}.textContent = ${cssString};` +
                 `document.head.appendChild(${style});`;
               let injectionPoint: number;
-              const wrapIdx = code.indexOf('System.register');
+              const wrapIdx = code.indexOf("System.register");
               const singleQuoteUseStrict = `'use strict';`;
               const doubleQuoteUseStrict = `"use strict";`;
               if (wrapIdx >= 0) {
-                const executeFnStart = code.indexOf('execute:', wrapIdx);
-                injectionPoint = code.indexOf('{', executeFnStart) + 1;
+                const executeFnStart = code.indexOf("execute:", wrapIdx);
+                injectionPoint = code.indexOf("{", executeFnStart) + 1;
               } else if (code.includes(singleQuoteUseStrict)) {
                 injectionPoint =
                   code.indexOf(singleQuoteUseStrict) +
@@ -261,17 +267,87 @@ const plugin = (): PluginOption[] => {
                   code.indexOf(doubleQuoteUseStrict) +
                   doubleQuoteUseStrict.length;
               } else {
-                throw new Error('Injection point for inlined CSS not found');
+                throw new Error("Injection point for inlined CSS not found");
               }
               s ||= new MagicString(code);
               s.appendRight(injectionPoint, injectCode);
             }
           } else {
+            /*
+            // @ts-expect-error
+            const publicAssetUrlMap = publicAssetUrlCache.get(config!)!;
+
+            // resolve asset URL placeholders to their built file URLs
+            const resolveAssetUrlsInCss = (
+              chunkCSS: string,
+              cssAssetName: string
+            ) => {
+              const encodedPublicUrls = config?.command === "build";
+
+              const relative = config?.base === "./" || config?.base === "";
+              const cssAssetDirname =
+                encodedPublicUrls || relative
+                  ? slash(getCssAssetDirname(cssAssetName))
+                  : undefined;
+
+              const toRelative = (filename: string) => {
+                // relative base + extracted CSS
+                const relativePath = normalizePath(
+                  path.relative(cssAssetDirname!, filename)
+                );
+                return relativePath[0] === "."
+                  ? relativePath
+                  : "./" + relativePath;
+              };
+
+              // replace asset url references with resolved url.
+              chunkCSS = chunkCSS.replace(
+                assetUrlRE,
+                (_, fileHash, postfix = "") => {
+                  const filename = this.getFileName(fileHash) + postfix;
+                  chunk.viteMetadata!.importedAssets.add(cleanUrl(filename));
+                  return encodeURIPath(
+                    toOutputFilePathInCss(
+                      filename,
+                      "asset",
+                      cssAssetName,
+                      "css",
+                      // @ts-expect-error
+                      config,
+                      toRelative
+                    )
+                  );
+                }
+              );
+              // resolve public URL from CSS paths
+              if (encodedPublicUrls) {
+                const relativePathToPublicFromCSS = normalizePath(
+                  path.relative(cssAssetDirname!, "")
+                );
+                chunkCSS = chunkCSS.replace(publicAssetUrlRE, (_, hash) => {
+                  const publicUrl = publicAssetUrlMap.get(hash)!.slice(1);
+                  return encodeURIPath(
+                    toOutputFilePathInCss(
+                      publicUrl,
+                      "public",
+                      cssAssetName,
+                      "css",
+                      // @ts-ignore
+                      config!,
+                      () => `${relativePathToPublicFromCSS}/${publicUrl}`
+                    )
+                  );
+                });
+              }
+              return chunkCSS;
+            };
+
             // resolve public URL from CSS paths, we need to use absolute paths
             chunkCSS = resolveAssetUrlsInCss(chunkCSS, getCssBundleName());
             // finalizeCss is called for the aggregated chunk in generateBundle
 
             chunkCSSMap.set(chunk.fileName, chunkCSS);
+            */
           }
         }
 
@@ -279,7 +355,7 @@ const plugin = (): PluginOption[] => {
           if (config?.build.sourcemap) {
             return {
               code: s.toString(),
-              map: s.generateMap({ hires: 'boundary' }),
+              map: s.generateMap({ hires: "boundary" }),
             };
           } else {
             return { code: s.toString() };
